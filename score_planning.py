94bd4ac0a8311d442b8f9f4a769a94ae750e1e90
marilene805 <517774@student.fontys.nl>
2023-09-15 13:38:21 +0200

    amper iets veranderd idk

---
+++ score_planning.py
@@ -0,0 +1,7 @@
+#functie om de score steeds te bepalen voor elke planning (di e voldoet aan de eisen), gebaseerd op de wensen.
+def score_planning(planning) -> int:
+    """ voor elke feasible solution bepalen we een score voor die planning. we zoeken de planning met de laagste score
+    arguments: planning? (2-opt heuristic is het verwisselen van twee dingen)
+    output: score int (we moeten verschil tussen de scores tussen het verschil meten, maar hoeft niet)
+    """
+    buren_samen = count() 
