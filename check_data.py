3543435297e6fe8141a0c5aee1ba8160a0e0b03f
marilene805 <517774@student.fontys.nl>
2023-09-19 19:45:22 +0200

    functie gemaakt

--- check_data.py
+++ check_data.py
@@ -3,17 +3,69 @@ from DataFrames import dataframes
 #################### hier zal ik functies proberen (m)
 df_bewoners, df_adressen, df_paren, df_buren, df_kookte_2021, df_tafelgenoot_2021 = dataframes('Running Dinner dataset 2022.xlsx')
 
-def check_allocation(kolom_van_sheet): #df_bewoners[0] in ons geval
+def controleer_lege_cellen(planning, kolommen):
     """
-    komt iedere persoon precies 1 keer voor in kolommen 'voor' 'hoofd' en 'na'
+    is iedereen een locatie voor elk gerecht toegewezen
+    arg: planning (feasible of infeasible)
+    output: niks of infeasible-melding
     """
-    lijst_bewoners = list(kolom_van_sheet)
-    for bewoner in lijst_bewoners:
-        count(bewoner)
-    return lijst_bewoners
-    
-print(check_allocation(df_bewoners.iloc[:, 0]))
+    df = pd.read_excel(planning) #mogelijk overbodige regel
+
+    for kolom in kolommen:
+        lege_cellen = df[df[kolom].isnull()]
+
+        if not lege_cellen.empty:
+            for index, rij in lege_cellen.iterrows():
+                lege_cel = rij[kolom]
+                print(f"Het is infeasible want cel '{kolom}' in rij {index + 2} is leeg. Inhoud: {lege_cel}")
+
+# Vervang 'jouw_excel_bestand.xlsx' door het pad naar jouw Excel-bestand
+planning = 'Running Dinner eerste oplossing 2022.xlsx'
+
+# Definieer de kolommen om te controleren op lege cellen
+kolommen_te_controleren = ['Voor', 'Hoofd', 'Na']
+
+# Roep de functie aan om de controle uit te voeren
+#print(controleer_lege_cellen(planning, kolommen_te_controleren))
 
 
 
+def controleer_adressen(df_koppels, planning):
+    """
+    controleert of paren altijd samen zijn.
+    """
+    # Laad de koppelgegevens in een DataFrame
+
+    # Laad de adresgegevens in een DataFrame
+    df_adressen = pd.read_excel(planning)
+
+    # Loop door de koppelgegevens en controleer adressen
+    for index, rij in df_koppels.iterrows():
+        persoon_a = rij['Bewoner1']
+        persoon_b = rij['Bewoner2']
+
+        adres_a_voor = df_adressen.loc[df_adressen['Bewoner'] == persoon_a, 'Voor'].values[0]
+        adres_b_voor = df_adressen.loc[df_adressen['Bewoner'] == persoon_b, 'Voor'].values[0]
+
+        adres_a_hoofd = df_adressen.loc[df_adressen['Bewoner'] == persoon_a, 'Hoofd'].values[0]
+        adres_b_hoofd = df_adressen.loc[df_adressen['Bewoner'] == persoon_b, 'Hoofd'].values[0]
+
+        adres_a_na = df_adressen.loc[df_adressen['Bewoner'] == persoon_a, 'Na'].values[0]        
+        adres_b_na = df_adressen.loc[df_adressen['Bewoner'] == persoon_b, 'Na'].values[0]
+
+        if adres_a_voor != adres_b_voor:
+            print(f"Fout: Het adres in 'Voor' voor {persoon_a} verschilt van dat voor {persoon_b}.")
+        
+        if adres_a_hoofd != adres_b_hoofd:
+            print(f"Fout: Het adres in 'Hoofd' voor {persoon_a} verschilt van dat voor {persoon_b}.")
+
+        if adres_a_na != adres_b_na:
+            print(f"Fout: Het adres in 'Na' voor {persoon_a} verschilt van dat voor {persoon_b}.")
+
+# Vervang de bestandspaden door de paden naar jouw Excel-bestanden
+
+
+# Roep de functie aan om de controle uit te voeren
+print(controleer_adressen(df_paren, planning))
+#print(df_paren)
 
