06f555caa71275fbaff6f4c48b5e44351b1b77c2
marilene805 <517774@student.fontys.nl>
2023-09-19 18:19:17 +0200

    svD\

--- check_data.py
+++ check_data.py
@@ -1,12 +1,19 @@
 import pandas as pd
 from DataFrames import dataframes
-#################### hier aal ik functies proberen (m)
-a =dataframes('Running Dinner dataset 2022.xlsx')
+#################### hier zal ik functies proberen (m)
+df_bewoners, df_adressen, df_paren, df_buren, df_kookte_2021, df_tafelgenoot_2021 = dataframes('Running Dinner dataset 2022.xlsx')
 
-def check_feasibility():
+def check_allocation(kolom_van_sheet): #df_bewoners[0] in ons geval
     """
-    is deplanning feasible
+    komt iedere persoon precies 1 keer voor in kolommen 'voor' 'hoofd' en 'na'
     """
+    lijst_bewoners = list(kolom_van_sheet)
+    for bewoner in lijst_bewoners:
+        count(bewoner)
+    return lijst_bewoners
     
+print(check_allocation(df_bewoners.iloc[:, 0]))
+
+
 
 
