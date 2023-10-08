1be229b3b8955cfdb93c43dd058a5d74f4d5995b
EllaCoolen <508779@student.fontys.nl>
2023-10-08 16:58:51 +0200

    dataframe in functie aangepast

--- alle_checks.py
+++ alle_checks.py
@@ -21,7 +21,7 @@ from eisen import *
         
         
 def alle_eisen(planning):
-    df_bewoners, df_adressen, df_paren, df_buren, df_kookte_2021, df_tafelgenoot_2021 = dataframes('Running Dinner dataset 2023 v2.xlsx')
+    df_paren = pd.read_excel('Running Dinner dataset 2023 v2.xlsx', skiprows=[0], sheet_name = 'Paar blijft bij elkaar')
     errors = 0
 
     # Voer alle eisencontroles uit zonder de lussen te verlaten
